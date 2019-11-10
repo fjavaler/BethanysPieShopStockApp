14dbf9283d44476078043a9cb5fddabb05d77136
Frederick Javalera <frederickjavalera@gmail.com>
2019-11-10 02:05:54 -0700

    Init commit

---
+++ BethanysPieShopStockApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BethanysPieShopStockApp.Model;
+using Xamarin.Forms;
+
+namespace BethanysPieShopStockApp
+{
+  // Learn more about making custom code visible in the Xamarin.Forms previewer
+  // by visiting https://aka.ms/xamarinforms-previewer
+  [DesignTimeVisible(false)]
+  public partial class MainPage : ContentPage
+  {
+    public MainPage()
+    {
+      InitializeComponent();
+
+      //Pie pie = new Pie();
+      //pie.Id = 1;
+      //pie.PieName = "Cherry pie";
+      //pie.Price = 20;
+
+      //Binding pieNameBinding = new Binding();
+      //pieNameBinding.Source = pie;
+      //pieNameBinding.Path = "PieName";
+      //NameEntry.SetBinding(Entry.TextProperty, pieNameBinding);
+
+      //Binding piePriceBinding = new Binding();
+      //piePriceBinding.Source = pie;
+      //piePriceBinding.Path = "Price";
+      //PriceEntry.SetBinding(Entry.TextProperty, piePriceBinding);
+    }
+  }
+}
---
+++ BethanysPieShopStockApp/Model/Pie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace BethanysPieShopStockApp.Model
+{
+  public class Pie: INotifyPropertyChanged
+  {
+    public int Id { get; set; }
+
+    public String PieName { get; set; }
+
+    public double Price { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+  }
+}
