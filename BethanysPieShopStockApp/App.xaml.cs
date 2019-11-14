488f23cb708ba2f3daf3ec18ed8a31b420f3d127
Frederick Javalera <frederickjavalera@gmail.com>
2019-11-14 13:42:00 -0700

    Added PieOverviewPage

--- BethanysPieShopStockApp/App.xaml.cs
+++ BethanysPieShopStockApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace BethanysPieShopStockApp
     {
       InitializeComponent();
 
-      MainPage = new MainPage();
+      MainPage = new PieOverviewPage();
     }
 
     protected override void OnStart()
---
+++ BethanysPieShopStockApp/PieOverviewPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace BethanysPieShopStockApp
+{
+  public partial class PieOverviewPage : ContentPage
+  {
+    public List<string> Pies { get; set; }
+
+    public PieOverviewPage()
+    {
+      InitializeComponent();
+
+      Pies = new List<string>()
+      {
+        "Apple Pie",
+        "Strawberry Cheese Cake",
+        "Strawberry Pie",
+        "Rhubarb Pie",
+        "Blueberry Cheese Cake",
+        "Cheese Cake",
+        "Cherry Pie",
+        "Christmas Apple Pie",
+        "Cranberry Pie",
+        "Peach Pie",
+        "Pumpkin Pie",
+      };
+
+      PiesListView.ItemsSource = Pies;
+    }
+  }
+}
