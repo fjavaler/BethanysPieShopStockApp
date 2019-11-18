1c3e4caf1650601ee8eaa6542d099f3f4a643d05
Frederick Javalera <frederickjavalera@gmail.com>
2019-11-18 15:17:40 -0700

    Left off at work.

---
+++ BethanysPieShopStockApp/Model/PieRepository.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace BethanysPieShopStockApp.Model
+{
+  public class PieRepository
+  {
+    public ObservableCollection<Pie> Collection { get; set; }
+    string imageUrl = "https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/x17/16800-blue-ribbon-apple-pie-600x600.jpg?ext=.jpg";
+
+    public PieRepository()
+    {
+      SetupRepository();
+    }
+
+    private void SetupRepository()
+    {
+      if (Collection == null)
+      {
+        Collection = new ObservableCollection<Pie>()
+        {
+          new Pie
+          {
+            PieName = "Apple Pie",
+            Price = 12.95,
+            Description = "Blah, blah, blah",
+            ImageUrl = imageUrl,
+            AvailableFromDate = new DateTime(2018, 12, 1),
+            InStock = false,
+          },
+          new Pie
+          {
+            PieName = "Strawberry Cheese Cake",
+            Price = 18.95,
+            Description = "Blah, blah, blah",
+            ImageUrl = imageUrl,
+            AvailableFromDate = new DateTime(2018, 12, 1),
+            InStock = false,
+          },
+          new Pie
+          {
+            PieName = "Strawberry Pie",
+            Price = 15.95,
+            Description = "Blah, blah, blah",
+            ImageUrl = imageUrl,
+            AvailableFromDate = new DateTime(2018, 12, 1),
+            InStock = true,
+          },
+          new Pie
+          {
+            PieName = "Rhubarb Pie",
+            Price = 15.95,
+            Description = "Blah, blah, blah",
+            ImageUrl = imageUrl,
+            AvailableFromDate = new DateTime(2018, 12, 1),
+            InStock = true,
+          },
+          new Pie
+          {
+            PieName = "Blueberry Cheese Cake",
+            Price = 18.95,
+            Description = "Blah, blah, blah",
+            ImageUrl = imageUrl,
+            AvailableFromDate = new DateTime(2018, 12, 1),
+            InStock = false,
+          },
+          new Pie
+          {
+            PieName = "Cheese Cake",
+            Price = 18.95,
+            Description = "Blah, blah, blah",
+            ImageUrl = imageUrl,
+            AvailableFromDate = new DateTime(2018, 12, 1),
+            InStock = true,
+          },
+          new Pie
+          {
+            PieName = "Cherry Pie",
+            Price = 15.95,
+            Description = "Blah, blah, blah",
+            ImageUrl = imageUrl,
+            AvailableFromDate = new DateTime(2018, 12, 1),
+            InStock = true,
+          },
+          new Pie
+          {
+            PieName = "Peach Pie",
+            Price = 15.95,
+            Description = "Blah, blah, blah",
+            ImageUrl = imageUrl,
+            AvailableFromDate = new DateTime(2018, 12, 1),
+            InStock = false,
+          },
+        };
+      }
+    }
+  }
+}
---
+++ BethanysPieShopStockApp/ViewModels/PieOverviewViewModel.cs
@@ -0,0 +1,52 @@
+﻿using BethanysPieShopStockApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace BethanysPieShopStockApp.ViewModels
+{
+  class PieOverviewViewModel : INotifyPropertyChanged
+  {
+    // Fields.
+    private readonly PieRepository pieRepository;
+    public event PropertyChangedEventHandler PropertyChanged;
+    public ObservableCollection<Pie> Pies { get; set; }
+
+    public PieOverviewViewModel()
+    {
+      pieRepository = new PieRepository();
+      Pies = pieRepository.Collection;
+    }
+
+    public void RaisePropertyChanged(string propertyName)
+    {
+      PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    ///// <summary>
+    ///// This method is executed when the AddPieButton is clicked.
+    ///// </summary>
+    ///// <param name="sender"></param>
+    ///// <param name="e"></param>
+    //private void AddPieButton_Clicked(object sender, EventArgs e)
+    //{
+    //  var newPie = new Pie
+    //  {
+    //    PieName = "Pumpkin Pie",
+    //    Price = 12.95,
+    //    Description = "Blah, blah, blah",
+    //    ImageUrl = imageUrl,
+    //    InStock = true,
+    //  };
+
+    //  Pies.Add(newPie);
+    //}
+
+    //private async void PiesListView_ItemTappedAsync(object sender, ItemTappedEventArgs e)
+    //{
+    //  await this.Navigation.PushAsync(new MainPage(e.Item as Pie));
+    //}
+  }
+}
